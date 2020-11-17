5d76eaa7f6a4b0a6b4cc361bf97770be5fa30ec8
Bartek <bartek.bober.koder@gmail.com>
2020-11-17 16:00:45 +0100

    add wait after message send

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,7 @@
 const { cfg: { url: { contacts }, constactPageCounter } } = require('./utils')
 const logger = require('../api/logger')
 const { messageLoop } = require('./messageSender')
-let sendMessageCount = {
-  i: 0
-}
+let counter = 0
 
 const openContacts = async (page) => {
   await page.goto(contacts, { waitUntil: 'domcontentloaded' })
@@ -19,11 +17,11 @@ const nextContactsPage = async (page) => {
 const runBot = async (page, runConfig) => {
   const limit = runConfig.messagesLimit > 0 ? runConfig.messagesLimit : 999
   await openContacts(page)
-  while(sendMessageCount.i <= limit) {
-    await messageLoop(page, runConfig)
+  while(counter <= limit) { // if limit will reach or users list will end 
+    await messageLoop(page, runConfig, counter)
     await nextContactsPage(page)
   }
-  
+  //TODO: followup
 }
 
-module.exports = { runBot, sendMessageCount }
+module.exports = { runBot }
--- bot/messageSender.js
+++ bot/messageSender.js
@@ -10,7 +10,6 @@ const {
     constactPageCounter 
   } 
 } = require('./utils')
-let { sendMessageCount } = require('./bot')
 
 const openProfile = async (page, profileLink) => { 
   logger.info(`Profile openend -> ${profileLink}`)
@@ -25,27 +24,24 @@ const openMessageWindow = async (page) => {
   logger.info('Message window opened')
 }
 
-const sendMessage = async (page, message) => {
+const sendMessage = async (page, message, user) => {
   await page.keyboard.type(message)
   await page.click(sendMessageBtn)
-  logger.http(`Message send to: `) // ${user}
+  logger.http(`Message send to: ${user.fullName}`)
   //TODO: add user to db
 }
 
-const messageLoop = async (page, runConfig) => {
-  const { message, followupMessage } = runConfig
+const messageLoop = async (page, runConfig, counter) => { 
   logger.info('Message loop')
-  //loop pages
+  
   const selectedUsers = await selectUserToSendMsg(page, runConfig) // select users from actual page
   for (const user of selectedUsers) {
-    console.log(user);
     await openProfile(page, user.profileHref)
     await openMessageWindow(page)
-    await sendMessage(page, message)
-    sendMessageCount.i++
+    await sendMessage(page, runConfig.runConfig.message, user)
+    counter++;
+    await page.waitFor(2 * 60 * 1000)
   }
-
-  //after message loop check followup
 }
 
 module.exports = { messageLoop }
