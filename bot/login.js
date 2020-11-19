68639736718195a4af817ebb0f82e3c20b4dcdc0
Bartek <bartek.bober.koder@gmail.com>
2020-11-19 15:39:50 +0100

    update

--- bot/login.js
+++ bot/login.js
@@ -29,19 +29,26 @@ const login = async (page, runConfig) => {
 
 const checkLogin = async (page) => {
   const url = await page.url()
-  if (url.includes('challenge')) {
+  logger.info(`Page: ${url}`)
+  if (!url.includes('challenge')) {
     const forms = await page.$$('form')
-    logger.info(`Forms ${forms.length}`)
-    forms.forEach(form => {
-      const htmlF = await page.evaluate(form, form.innerHtml, form)  
-      logger.info(`${htmlF}`)
-    });
+    console.log(`Forms ${forms.length}`)
+    // for(const form of forms) {
+    //   // const form = forms[0]
+    //   const htmlF = await page.evaluate(form, form.innerHTML, form)  
+    //   logger.info(`${htmlF}`)
+      const tt1 = await page.$('form:nth-child(1)')
+      const tt2 = await page.$$('form:nth-child(2)')
+      const el1 = await page.evaluate(tt1 => tt1.innerHTML, tt1)
+      const el2 = await page.evaluate(tt2 => tt2.innerHTML, tt2)
+      logger.info(el1)
+      logger.info(el2)
+    // }
   }
     
     // const passwords = await page.$$('input[type=password]')
 
-    logger.info(`forms : ${forms.length} | passwords inputs: ${passwords.length}`)
-  }
+    // logger.info(`forms : ${forms.length} | passwords inputs: ${passwords.length}`)
 }
 
 module.exports = { login, checkLogin }
