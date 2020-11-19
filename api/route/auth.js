9f1f143ae45554d57f083050673971aff3b40c9f
Bartek <bartek.bober.koder@gmail.com>
2020-11-19 09:07:07 +0100

    add auth logs

--- api/route/auth.js
+++ api/route/auth.js
@@ -4,11 +4,14 @@ const authModel = require('../model/authModel')
 const logger = require('../logger')
 
 router.post('/', async (req, res) => {
+  logger.info('into auth 1')
   if (req.body.authKey === process.env.API_AUTH_KEY) {
+    logger.info('into auth 1')
     const auth = new authModel({
       authType: req.body.type,
       key: req.body.authKey
     })
+    logger.info('into auth 2')
 
     await auth.save()
     .then(() => {
@@ -21,11 +24,13 @@ router.post('/', async (req, res) => {
         res.status(500).json({ msg: 'Error', ...err })
       }
     })
+    logger.info('into auth 1')
 
   } else {
     logger.error('Wrong auth key.')
     res.status(500).json({ msg: 'Wrgong auth key' })
   }
+  logger.info('into auth 1')
 })
 
 module.exports = router
